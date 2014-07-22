3413cb92c778e7878b21c0689fe51d102a65a1b1
Jesse Bilsten <jesse@bilsten.net>
2014-07-22 15:16:11 -0700

    No longer need to add, checked into repo

--- install-deps.sh
+++ install-deps.sh
@@ -4,6 +4,3 @@
 # Janus VIM framework
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 
-# Install VIM plugins
-mkdir ~/.janus
-git clone https://github.com/bling/vim-airline ~/.janus/vim-airline # Powerline-esque VIM Status bar
