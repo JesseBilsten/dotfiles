4c6e9cd6ac73b22db27c739cd4491741d5a6806e
Jesse Bilsten <jesse@bilsten.net>
2014-08-01 10:31:22 -0700

    WIP symlink

--- symlink.sh
+++ symlink.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+# Work in progress...
 
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	for f in ~/dotfiles/*
+	do
+		ln -s "$f" "$HOME/.${f##*/}"
+	done
+
+	#rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		#--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
+
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
